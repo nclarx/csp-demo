a488cd5be61d870410e0cc3c18b64bf295124403
Nick Clark <nick@clarx.com.au>
2019-08-02 13:35:19 +1000

    FIX: removed meta csp from index.html

--- response-header/server.py
+++ response-header/server.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 def hello():
     page = render_template('./index.html')
     response = Response(page)
-    response.headers['Content-Security-Policy'] = "default-src 'self';" + \
-            "style-src 'self';" + \
-            "script-src 'self';" + \
-            "img-src 'self;"
+    response.headers['Content-Security-Policy'] = "default-src 'self'; " + \
+            "style-src 'self'; " + \
+            "script-src 'self'; " + \
+            "img-src 'self; "
     return response
 
 if __name__ == "__main__":
