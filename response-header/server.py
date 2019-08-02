8d0cec81c8bb68f650f23ddfcb73b1f1f7b31c1c
Nick Clark <nick@clarx.com.au>
2019-08-02 14:27:08 +1000

    UPDATE: added iframe stuff

--- response-header/server.py
+++ response-header/server.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, Response
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     page = render_template('./index.html')
     response = Response(page)
     response.headers['Content-Security-Policy'] = "default-src 'self'; " + \
-            "style-src 'self'; " + \
-            "script-src 'self'; " + \
-            "img-src 'self; "
+        "style-src 'self' 'unsafe-inline' https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css; " + \
+        "script-src 'self' 'unsafe-inline' https://code.jquery.com/jquery-3.3.1.slim.min.js https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js; " + \
+        "img-src 'none'; " + \
+        "report-uri https://clarx.dev/dev/null;" + \
+        "frame-src 'none';"
     return response
+
 
 if __name__ == "__main__":
     app.run(host='localhost', port=8200)
-
